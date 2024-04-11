e49d651d1eb0300db10fbf7dd7adad475a12f4e6
Lexia <Clarisse787878@gmail.com>
2024-04-11 18:41:35 +0100

    Index + Footer

--- src/pages/FooterApp.jsx
+++ src/pages/FooterApp.jsx
@@ -1,31 +1,91 @@
 import { Footer } from "flowbite-react";
 import { Link } from "react-router-dom";
 
+import { BsDribbble, BsFacebook, BsGithub, BsInstagram, BsTwitter } from "react-icons/bs";
+
+
 const FooterApp = () => {
+  // return (
+  //   <Footer container>
+  //     <div className="w-full">
+  //       <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
+  //         <div>
+  //           <Footer.Brand
+  //             href="https://flowbite.com"
+  //             src="https://flowbite.com/docs/images/logo.svg"
+  //             alt="Flowbite Logo"
+  //             name="Flowbite"
+  //           />
+  //         </div>
+  //         <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
+  //           <div>
+  //             <Footer.Title title="about" />
+  //             <Footer.LinkGroup col>
+  //               <Footer.Link href="#">Flowbite</Footer.Link>
+  //               <Footer.Link href="#">Tailwind CSS</Footer.Link>
+  //             </Footer.LinkGroup>
+  //           </div>
+  //           <div>
+  //             <Footer.Title title="Follow us" />
+  //             <Footer.LinkGroup col>
+  //               <Footer.Link href="#">Github</Footer.Link>
+  //               <Footer.Link href="#">Discord</Footer.Link>
+  //             </Footer.LinkGroup>
+  //           </div>
+  //           <div>
+  //             <Footer.Title title="Legal" />
+  //             <Footer.LinkGroup col>
+  //               <Footer.Link href="#">Privacy Policy</Footer.Link>
+  //               <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
+  //             </Footer.LinkGroup>
+  //           </div>
+  //         </div>
+  //       </div>
+  //       <Footer.Divider />
+  //       <div className="w-full sm:flex sm:items-center sm:justify-between">
+  //         <Footer.Copyright href="#" by="Flowbite™" year={2022} />
+  //         <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+  //           <Footer.Icon href="#" icon={BsFacebook} />
+  //           <Footer.Icon href="#" icon={BsInstagram} />
+  //           <Footer.Icon href="#" icon={BsTwitter} />
+  //           <Footer.Icon href="#" icon={BsGithub} />
+  //           <Footer.Icon href="#" icon={BsDribbble} />
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </Footer>
+  // );
+
   return (
-    <Footer>
-      <div>
-        <div className="w-full text-center">
-          <div className="w-full justify-between sm:flex sm:items-center sm:justify-between">
-            <Footer.Brand>
-              <Link to="/">
-                <img
-                  src="../public/images/website-logo-sample.png"
-                  className="mr-3 h-6 sm:h-9"
-                  alt="Sample website logo"
-                />
-              </Link>
-            </Footer.Brand>
-            <Footer.LinkGroup>
-              <Footer.Link href="#">About</Footer.Link>
-              <Footer.Link href="#">Privacy Policy</Footer.Link>
-              <Footer.Link href="#">Licensing</Footer.Link>
-              <Footer.Link href="#">Contact</Footer.Link>
+    <Footer className="bg-custom-blue text-white rounded-none">
+      <div className="w-full text-center">
+        <div className="w-full justify-between sm:flex sm:items-center sm:justify-center">
+
+          <div className="flex flex-col items-center justify-center pt-9" style={{ justifyContent: "center" }}>
+            <h2>Lucile Pelou</h2>
+            <h3>IT Student</h3>
+            <Footer.LinkGroup className="footer-links pt-9 pb-9 gap-4">
+              <Footer.Link style={{ textDecoration: 'underline', textUnderlineOffset: '0.2em', color: "#75C4EA", fontSize: "20px" }} href="#">Home</Footer.Link>
+              <Footer.Link style={{ textDecoration: 'underline', textUnderlineOffset: '0.2em', color: "#75C4EA", fontSize: "20px" }} href="portfolio/index.html">Portfolio</Footer.Link>
+              <Footer.Link style={{ textDecoration: 'underline', textUnderlineOffset: '0.2em', color: "#75C4EA", fontSize: "20px" }} href="store/index.html">Store</Footer.Link>
+              <Footer.Link style={{ textDecoration: 'underline', textUnderlineOffset: '0.2em', color: "#75C4EA", fontSize: "20px" }} href="contact/index.html">Contact</Footer.Link>
+            </Footer.LinkGroup>
+            <Footer.LinkGroup className="footer-icons pb-9 gap-3">
+              <Footer.Link style={{ color: "#7DD3FC" }} href="https://www.linkedin.com/in/lucile-pelou/" target="_blank">
+                <i className="fab fa-linkedin text-3xl"></i>
+              </Footer.Link>
+              <Footer.Link style={{ color: "#7DD3FC" }} href="https://github.com/Clarisse78" target="_blank">
+                <i className="fab fa-github-square text-3xl"></i>
+              </Footer.Link>
+              <Footer.Link style={{ color: "#7DD3FC" }} href="https://discord.com/users/377784448243990538" target="_blank">
+                <i className="fa-brands fa-discord text-3xl"></i>
+              </Footer.Link>
             </Footer.LinkGroup>
           </div>
-          <Footer.Divider />
-          <Footer.Copyright href="#" by="Flowbite™" year={2022} />
         </div>
+        <p className="privacy pb-11">
+          <Footer.Link style={{ textDecoration: 'underline', textUnderlineOffset: '0.2em', color: "#35AADF", fontSize: "20px" }} href="privacy.html">Privacy &amp; Legal</Footer.Link>
+        </p>
       </div>
     </Footer>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Badge from '@mui/material/Badge';
 import { Link } from "react-router-dom";
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 
 const Home = () => {
   return (
@@ -44,34 +45,64 @@ const Home = () => {
           </Box>
         </Box>
       </div>
-      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '1356px' }} className="theme-dark section-selector-1">
-        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '820px' }} className="col-1 text-center">
-          <Typography variant="h2">Recent projects</Typography>
-          <Typography variant="h3">
-            Below you can see some examples of my recent work. Check out my complete portfolio of
-            <Link to="/portfolio">web design</Link> projects. Have a project you would like to discuss?
-            <Link to="/contact">Let's make something great together!</Link>
+      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} className="theme-dark section-selector-1">
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} className="col-1 text-center">
+          <Typography variant="h2" sx={{ fontSize: "42px", color: "white", fontFamily: 'Roboto, sans-serif', marginBottom: "1%" }}>Recent projects</Typography>
+          <Typography variant="h3" sx={{
+            fontSize: "28px", marginLeft: "20%", marginRight: "20%",
+            color: "#cbd4d8", fontFamily: 'Roboto, sans-serif', lineHeight: "1.6"
+          }}>
+            Below you can see some examples of my recent work. Check out my complete portfolio of {" "}
+            <Link to="/portfolio" style={{ textDecoration: 'underline', textUnderlineOffset: '0.2em', color: "Highlight" }}>web design</Link> projects. Have a project you would like to discuss? {""}
+            <Link to="/contact" style={{ textDecoration: 'underline', color: "Highlight", textUnderlineOffset: '0.2em' }}>Let's make something great together!</Link>
           </Typography>
         </Box>
 
-        <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }} className="flex-cols-3 cols-gap-4">
-          <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} className="col-3">
-            <Box sx={{ overflow: 'hidden' }} className="photos-zoom photos-brightness">
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '100%', padding: "5%" }} className="flex-cols-3 cols-gap-4">
+          <Box sx={{ display: 'flex', flexDirection: 'column', maxWidth: "400px", gap: "5px" }} className="col-3">
+            <Box sx={{ overflow: 'hidden', paddingBottom: "2%" }} className="photos-zoom photos-brightness">
               <Link to="https://clarisse78.github.io/Lexia/">
-                <img src="assets/img/project-dev-portfolio-min.png" alt="Placeholder image" />
+                <img src="../public/images/project-dev-portfolio-min.png" alt="Placeholder image" />
               </Link>
             </Box>
-            <Typography variant="h3">Development Portfolio</Typography>
-            <Typography variant="body1">An another website Portfolio for my development projects.</Typography>
-            <Button variant="solid" color="soft" href="https://clarisse78.github.io/Lexia/">
-              View project
+            <Typography variant="h3" className="text-white" sx={{ fontSize: "25px", fontWeight: "700", fontFamily: 'Roboto, sans-serif' }}>Development Portfolio</Typography>
+            <Typography variant="body1" className="text-white" sx={{ fontSize: "20px", overflowWrap: "break-word", flexGrow: "1", fontFamily: 'Roboto, sans-serif' }}>An another website Portfolio for my development projects.</Typography>
+            <Button variant="solid" color="soft" href="https://clarisse78.github.io/Lexia/" style={{ backgroundColor: 'white', color: 'black', border: '1px solid black', width: "fit-content", fontWeight: "bold", marginTop: "2%" }}>
+              <Typography sx={{ fontSize: "18px", fontWeight: "600" }}>View project</Typography>
+              <ArrowRightAltIcon sx={{ paddingLeft: "2%" }}></ArrowRightAltIcon>
             </Button>
           </Box>
 
-          {/* Repeat the above Box for other projects... */}
+          <Box sx={{ display: 'flex', flexDirection: 'column', maxWidth: "400px", gap: "5px" }} className="col-3">
+            <Box sx={{ overflow: 'hidden', paddingBottom: "2%" }} className="photos-zoom photos-brightness">
+              <Link to="https://godmelcorp.github.io/">
+                <img src="../public/images/project-proxima-min.png" alt="Placeholder image" />
+              </Link>
+            </Box>
+            <Typography variant="h3" className="text-white" sx={{ fontSize: "25px", fontWeight: "700", fontFamily: 'Roboto, sans-serif' }}>Proxima's Quest</Typography>
+            <Typography variant="body1" className="text-white" sx={{ fontSize: "20px", overflowWrap: "break-word", flexGrow: "1", fontFamily: 'Roboto, sans-serif' }}>The Website of a video games project made in C# with Unity. It present the game and the creators.</Typography>
+            <Button variant="solid" color="soft" href="https://clarisse78.github.io/Lexia/" style={{ backgroundColor: 'white', color: 'black', border: '1px solid black', width: "fit-content", fontWeight: "bold", marginTop: "2%" }}>
+              <Typography sx={{ fontSize: "18px", fontWeight: "600" }}>View project</Typography>
+              <ArrowRightAltIcon sx={{ paddingLeft: "2%" }}></ArrowRightAltIcon>
+            </Button>
+          </Box>
+          <Box sx={{ display: 'flex', flexDirection: 'column', maxWidth: "400px", gap: "5px" }} className="col-3">
+            <Box sx={{ overflow: 'hidden', paddingBottom: "2%" }} className="photos-zoom photos-brightness">
+              <Link to="https://clarisse78.github.io/WebDesignPortfolio/portfolio/js/index.html">
+                <img src="../public/images/project-javascript-min.png" alt="Placeholder image" />
+              </Link>
+            </Box>
+            <Typography variant="h3" className="text-white" sx={{ fontSize: "25px", fontWeight: "700", fontFamily: 'Roboto, sans-serif' }}>Javascript Projects</Typography>
+            <Typography variant="body1" className="text-white" sx={{ fontSize: "20px", overflowWrap: "break-word", flexGrow: "1", fontFamily: 'Roboto, sans-serif' }}>A selection of my recent JavaScript projects.</Typography>
+            <Button variant="solid" color="soft" href="https://clarisse78.github.io/Lexia/" style={{ backgroundColor: 'white', color: 'black', border: '1px solid black', width: "fit-content", fontWeight: "bold", marginTop: "2%" }}>
+              <Typography sx={{ fontSize: "18px", fontWeight: "600" }}>View project</Typography>
+              <ArrowRightAltIcon sx={{ paddingLeft: "2%" }}></ArrowRightAltIcon>
+
+            </Button>
+          </Box>
         </Box>
       </Box>
-    </Box>
+    </Box >
   );
 };
 
